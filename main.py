c1646611b328becdf7e42f03e331ced0615f4c13
Kabieee <lzfengyyy@gmail.com>
2020-02-28 17:12:24 +0800

    取消token

--- main.py
+++ main.py
@@ -21,33 +21,33 @@ import traceback
 
 app = Flask(__name__)
     
-@app.before_request
-def before():
-    if(request.path == '/query/'):
-        # return request.path;
-        try:
-            request_time = int(request.args.get('ts'))
-            server_time = int(time.time())
-            request_token = request.args.get('tk')
+# @app.before_request
+# def before():
+#     if(request.path == '/query/'):
+#         # return request.path;
+#         try:
+#             request_time = int(request.args.get('ts'))
+#             server_time = int(time.time())
+#             request_token = request.args.get('tk')
 
-            diff_time = abs(server_time-request_time)
-            # if diff_time > 3:
-            #     return json.dumps({'errmsg':'请求参数不正确','status':'500','desc':''})
-            sha1 = hashlib.sha1()
-            sha1.update('feiren_{0}'.format(request_time).encode('utf8'))
-            server_token = sha1.hexdigest()
-            # print(server_token,'feiren_{0}'.format(request_time))
-            if server_token == request_token:
-                return None
-            else:
-                return json.dumps({'errmsg':'token不正确','status':500,'desc':''})
+#             diff_time = abs(server_time-request_time)
+#             # if diff_time > 3:
+#             #     return json.dumps({'errmsg':'请求参数不正确','status':'500','desc':''})
+#             sha1 = hashlib.sha1()
+#             sha1.update('feiren_{0}'.format(request_time).encode('utf8'))
+#             server_token = sha1.hexdigest()
+#             # print(server_token,'feiren_{0}'.format(request_time))
+#             if server_token == request_token:
+#                 return None
+#             else:
+#                 return json.dumps({'errmsg':'token不正确','status':500,'desc':''})
 
-        except Exception as e:
-            return json.dumps({
-                'errmsg':str(e),
-                'status':500,
-                'desc':''
-            })
+#         except Exception as e:
+#             return json.dumps({
+#                 'errmsg':str(e),
+#                 'status':500,
+#                 'desc':''
+#             })
 
 @app.after_request
 def after(response):
@@ -57,8 +57,11 @@ def after(response):
 
 @app.route('/query/',methods=['POST','GET'])
 def query():
-        ex_code = '70256206847223'
-        ex_type = 'huitongkuaidi'
+    try:
+        request_data = request.get_json()
+        print(request_data['ex_code'])
+        ex_code = request_data['ex_code']
+        ex_type = request_data['ex_type']
         q = Query('https://www.kuaidi100.com',ex_type,ex_code)
         kuaidi_data = q.query_express()
         data_dict = json.loads(kuaidi_data)
@@ -71,6 +74,12 @@ def query():
         }
         response = make_response(json.dumps(new_dict,ensure_ascii=False))
         return response 
+    except Exception as e:
+        return json.dumps({
+            'errmsg':str(e),
+            'status':500,
+            'desc':''
+        })
 
     
 
